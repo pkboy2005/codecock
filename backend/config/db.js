74227f9e91f682312c248d01fef94af3f2680348
pkboy2005 <Pkboy2005@gmail.com>
2025-02-18 21:53:16 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,16 +2,16 @@ require("dotenv").config(); // Load environment variables
 
 const mongoose = require("mongoose");
 
-const MONGO_URL = process.env.MONGODB_URL; // Read from .env file
+const MONGODB_URL = process.env.MONGODB_URL; // Read from .env file
 
-if (!MONGO_URL) {
+if (!MONGODB_URL) {
   console.error("❌ MONGODB_URL is not defined! Check your .env file.");
   process.exit(1);
 }
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(MONGO_URL, {
+    await mongoose.connect(MONGODB_URL, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
