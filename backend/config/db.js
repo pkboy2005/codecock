dff0a973c693afa68ad4bc2915e12a948dabf84d
pkboy2005 <Pkboy2005@gmail.com>
2025-02-18 06:23:12 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
 
-    await mongoose.connect("mongodb://127.0.0.1:27017/codeIDE",{
+    await mongoose.connect("mongodb+srv://coder7650:JOIUOykLvO20cnD2@cluster0.x4to4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",{
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
