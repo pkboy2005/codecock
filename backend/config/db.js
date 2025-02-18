1552db26c7b9037617f91cc8b5e5a9969318ecf4
pkboy2005 <Pkboy2005@gmail.com>
2025-02-18 21:58:18 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,24 +1,18 @@
-require("dotenv").config(); // Load environment variables
-
 const mongoose = require("mongoose");
-
-const MONGODB_URL = process.env.MONGODB_URL; // Read from .env file
-
-if (!MONGODB_URL) {
-  console.error("❌ MONGODB_URL is not defined! Check your .env file.");
-  process.exit(1);
-}
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(MONGODB_URL, {
+    const MONGO_URL = "mongodb+srv://coder7650:JOIUOykLvO20cnD2@cluster0.x4to4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+    await mongoose.connect(MONGO_URL,{
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    console.log("✅ MongoDB connected");
+    // mongoose.connect("mongodb://127.0.0.1:27017/codeIDE");
+    console.log("MongoDB connected");
+    console.log(process.env.MONGODB_URL);
+    
   } catch (error) {
-    console.error("❌ MongoDB not connected:", error);
-    process.exit(1);
+    console.log("MongoDB not connected",error)
   }
 };
 
