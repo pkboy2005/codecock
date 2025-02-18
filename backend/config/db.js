94a9b6c21308411aef0880611c2a3f5f401349cc
pkboy2005 <Pkboy2005@gmail.com>
2025-02-18 06:45:45 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,15 +2,16 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-
-    await mongoose.connect("mongodb+srv://coder7650:JOIUOykLvO20cnD2@cluster0.x4to4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",{
+    const MONGO_URL = "mongodb+srv://coder7650:JOIUOykLvO20cnD2@cluster0.x4to4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+    await mongoose.connect(MONGO_URL,{
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
+    // mongoose.connect("mongodb://127.0.0.1:27017/codeIDE");
     console.log("MongoDB connected");
     
   } catch (error) {
-    console.log(error)
+    console.log("MongoDB not connected",error)
   }
 };
 
